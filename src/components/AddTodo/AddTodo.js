25c868ef5c22dd2ee6f8c2274fc9d591b16527e4
Aleksandra Chyła <aleks.chyla@gmail.com>
2019-05-08 12:32:16 +0200

    AppBar added

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,13 +1,83 @@
-import React, {Component} from 'react';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
 
-class AddTodo extends Component {
+const styles = theme => ({
+    root: {
+        width: '100%',
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing.unit * 3,
+            width: 'auto',
+        },
+    },
+    inputRoot: {
+        color: 'inherit',
+        width: '100%',
+    },
+    inputInput: {
+        paddingTop: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 750,
+        },
+    },
+    toolbar: {
+        margin: '0 auto',
+    }
+});
+
+class AddTodo extends React.Component {
+
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar className={classes.toolbar}>
+                        <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                            TodoList
+                        </Typography>
+                        <div className={classes.search}>
+                            <InputBase
+                                placeholder="Dodaj…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                            />
+                        </div>
+                        <div className={classes.grow} />
+                    </Toolbar>
+                </AppBar>
             </div>
         );
     }
 }
 
-export default AddTodo;
+export default withStyles(styles)(AddTodo);
